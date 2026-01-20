ef99a8c5287212e1020ab85efe0704dc9fe57447
EdderFlores <floresmoy11@gmail.com>
2026-01-19 21:59:33 -0600

    added old excercises to main public GItHub Repo

---
+++ ejercicios_de_intro_a_python.py
@@ -0,0 +1,7 @@
+print ('This is my second Python program!')
+print ('I am learning Python programming.')
+print ('Python is fun!')
+
+print (1+1)
+print (90+430)
+print (4*5)
---
+++ ejercicios_extra_de_intro_python.py
@@ -0,0 +1,19 @@
+full_name = "Edder Luis Moises Flores Salinas"
+age = 26
+favorite_color = "Gold"
+favorite_food = "Burgers"
+age_in_10_years = age + 10
+
+print (f"Hi, my name is {full_name}")
+print (f"I'm {age} years old")
+print (f"My favorite color is {favorite_color}")
+print (f"My favorite food is {favorite_food}") 
+
+print (f"In about 10 years I'll be {age_in_10_years} years old")
+
+
+full_meters = 25
+
+full_centimeters = full_meters * 100
+
+print (f"{full_meters} meters are {full_centimeters} centimeters")
