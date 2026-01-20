e50483b331e889e57113a362248b09f1dc0a140b
EdderFlores <floresmoy11@gmail.com>
2026-01-20 00:29:35 -0600

    started complementary execercises for python

---
+++ ejercicios_de_sintaxis.py
@@ -0,0 +1,12 @@
+my_string = "This is my main string"
+my_second_string = "This is my second string"
+my_integer = 45
+my_list = [
+    "first element",
+    "second element",
+    "third element"
+]
+my_float = 3.1416
+my_boolean = True
+
+print("Strings addition:" + {my_string} + ' ' + {my_second_string})
