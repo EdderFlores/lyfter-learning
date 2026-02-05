d5ab69c8de92787547aed49d15d9e6f9bca221ef
EdderFlores <floresmoy11@gmail.com>
2026-02-05 09:15:46 -0600

    iterating practices

---
+++ testing_live.py
@@ -0,0 +1,7 @@
+my_favorite_records=[
+'Dark Side Of The Moon',
+'Fear of a Blank Planet',
+'Signify',
+]
+for index, record in enumerate(my_favorite_records):
+    print(f'Record {index}: {record}')
